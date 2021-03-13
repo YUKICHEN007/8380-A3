7651ddf0f60948b5a0199dc3feb69984a3ce5823
YUKICHEN007 <yizhuangchen@unomha.edu>
2021-03-13 12:56:05 -0600

    heruko setup

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'v@%mah&g6b_5in5rj4qsgjs_zk&l4d=no#&y+nm*s%v)rn6k9f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -155,3 +156,5 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
